e8c2978de64bdff7bc2c9779db439ee9f61262c2
vishakhachavan-07 <vishakhachavan964@gmail.com>
2026-01-01 10:58:06 +0530

    commit

---
+++ TABLE BAISIC.c
@@ -0,0 +1,38 @@
+<table border="1">
+    <tr>
+        <th>NAME</th>
+        <th>AGE</th>
+        <th>CITY</th>
+        <th>COLLEGE</th>
+        <th>YEAR</th>
+        <th>BRANCH</th>
+        <th>ENROLL NO</th>
+    </tr>
+    <tr>
+        <td>VISHAKHA CHAVAN</td>
+        <td>19</td>
+        <td>VASAI(W)</td>
+        <td>VCET</td>
+        <td>SECOND YEAR</td>
+        <td>COMPUTER ENGINEERING</td>
+        <td>22</tr>
+    </tr>
+    <tr>
+        <td>SWAMINI BHILUNGDW</td>
+        <td>20</td>
+        <td>NAIGOAN(W)</td>
+        <td>VCET</td>
+        <td>SECOND YEAR</td>
+        <td>COMPUTER ENGINEERING</td>
+        <td>12</tr>
+    </tr>
+     <tr>
+        <td>SAUMIT BHOJNE</td>
+        <td>20</td>
+        <td>NALLASOPARA(E)</td>
+        <td>VCET</td>
+        <td>SECOND YEAR</td>
+        <td>COMPUTER ENGINEERING</td>
+        <td>13</tr>
+    </tr>
+</table>
